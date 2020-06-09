d5c5ca8a4ead023c527d3bccf0a930cd8fe839d6
Lakshmi Sirisha S <lakshmi.siri299@gmail.com>
2020-06-09 13:04:06 +0530

    intial commit

---
+++ models/content.model.js
@@ -0,0 +1,19 @@
+const mongoose= require('mongoose');
+const Schema = mongoose.Schema;
+const contentSchema = new Schema({
+    title:{type: String, required: true, unique: true},
+    description: {type: String, required: true}, 
+    duration: {type: Number, required:true},
+    date:{type:Date, required:true},
+    cast:{type: String},
+    producedBy:{type: String},
+    director:{type: String},
+    music:{type: String},
+    language:{type: String}
+},{
+timestamps: true,
+});
+
+const Content = mongoose.model('Content', contentSchema);
+
+module.exports = Content;
